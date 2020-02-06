0600ae6d383819b2330ffb73822037e9dc7bca58
SamTheWarlock <43090976+SamTheWarlock@users.noreply.github.com>
2020-02-06 18:12:04 +0000

    Update psssys.py

--- psssys.py
+++ psssys.py
@@ -1,135 +1,146 @@
-def redoAO():
-    print("Perform an action:")
-    print("1. Change a password.")
-    print("2. Delete an account")
-    print("3. Log Out")
-    inp = input("What would you like to do? Enter a number: ")
-    if inp == "1":
-        changeacc = input("Which account would you like to modify the password on?: ")
-        if changeacc == logins.a[0]:
-            print("To what?: ")
-            passwordin = input("Enter a password: ")
-            logins.a[1] = passwordin
-            print("Password Set.")
-            redoAO()
-        elif changeacc == logins.c[0]:
-            print("To what?: ")
-            passwordin = input("Enter a password: ")
-            logins.c[1] = passwordin
-            print("Password Set.")
-            redoAO()
-        elif changeacc == logins.d[0]:
-            print("To what?: ")
-            passwordin = input("Enter a password: ")
-            logins.d[1] = passwordin
-            print("Password Set.")
-            redoAO()
-        elif changeacc == logins.e[0]:
-            print("To what?: ")
-            passwordin = input("Enter a password: ")
-            logins.e[1] = passwordin
-            print("Password Set.")
-            redoAO()
-        elif changeacc == logins.f[0]:
-            print("To what?: ")
-            passwordin = input("Enter a password: ")
-            logins.f[1] = passwordin
-            print("Password Set.")
-            redoAO()
-        elif changeacc == logins.b[0]:
-            print("You cannot change the password of an actively logged in user.")
-            redoAO()
-        else:
-            print("User undefined.")
-            redoAO()
-    elif inp == "2":
-        who = input("Who would you like to delete?: ")
-        if who == logins.a[0]:
-            print("Are you sure? Press the enter key to confirm.")
-            input(">>")
-            logins.a[0] = False
-            logins.a[1] = False
-            print("Account Deleted. (Re-run the IDLE to re-allow access)")
-            redoAO()
-        elif who == logins.b[0]:
-            print("Admin user is un-deletable.")
-            redoAO()
-        else:
-            print("User undefined.")
-            redoAO()
-    elif inp == "3":
-        print("Successfully logged out as Admin.")
-        retry()
-    else:
-        print("Action undefined.")
-        redoAO()
-            
+from flask import Flask
+from flask import render_template
+from flask import request
 
-class logins:
-    a = ["sam","password"]
-    b = ["admin","admin123"]
-    c = ["local","local"]
-    d = ["alex","yeetethsksksksksk"]
-    e = ["false","false"]
-    f = ["false","false"]
+app = Flask(__name__)
 
-def retry():
-    name = input("What is your username?: ")
-
-    if name.lower() == logins.a[0]:
-         pasw = input("Please enter your password: ")
-         if pasw == logins.a[1]:
-            print("Welcome!")
-         else:
-            print("Incorrect password.")
+@app.route("/", methods=['GET', 'POST'])
+def application():
+    def redoAO():
+        return("Perform an action:")
+        return("1. Change a password.")
+        return("2. Delete an account")
+        return("3. Log Out")
+        inp = input("What would you like to do? Enter a number: ")
+        if inp == "1":
+            changeacc = input("Which account would you like to modify the password on?: ")
+            if changeacc == logins.a[0]:
+                return("To what?: ")
+                passwordin = input("Enter a password: ")
+                logins.a[1] = passwordin
+                return("Password Set.")
+                redoAO()
+            elif changeacc == logins.c[0]:
+                return("To what?: ")
+                passwordin = input("Enter a password: ")
+                logins.c[1] = passwordin
+                return("Password Set.")
+                redoAO()
+            elif changeacc == logins.d[0]:
+                return("To what?: ")
+                passwordin = input("Enter a password: ")
+                logins.d[1] = passwordin
+                return("Password Set.")
+                redoAO()
+            elif changeacc == logins.e[0]:
+                return("To what?: ")
+                passwordin = input("Enter a password: ")
+                logins.e[1] = passwordin
+                return("Password Set.")
+                redoAO()
+            elif changeacc == logins.f[0]:
+                return("To what?: ")
+                passwordin = input("Enter a password: ")
+                logins.f[1] = passwordin
+                return("Password Set.")
+                redoAO()
+            elif changeacc == logins.b[0]:
+                return("You cannot change the password of an actively logged in user.")
+                redoAO()
+            else:
+                return("User undefined.")
+                redoAO()
+        elif inp == "2":
+            who = input("Who would you like to delete?: ")
+            if who == logins.a[0]:
+                return("Are you sure? Press the enter key to confirm.")
+                input(">>")
+                logins.a[0] = False
+                logins.a[1] = False
+                return("Account Deleted. (Re-run the IDLE to re-allow access)")
+                redoAO()
+            elif who == logins.b[0]:
+                return("Admin user is un-deletable.")
+                redoAO()
+            else:
+                return("User undefined.")
+                redoAO()
+        elif inp == "3":
+            return("Successfully logged out as Admin.")
             retry()
-    elif name.lower() == logins.b[0]:
-        print("Welcome,",name)
-        pasw = input("Please enter your password: ")
-        if pasw == logins.b[1]:
-            print("Welcome")
-            adminloggedin = True
+        else:
+            return("Action undefined.")
             redoAO()
-        else:
-            print("Incorrect password.")
-            retry()
-    elif name.lower() == logins.c[0]:
-        print("Welcome,",name)
-        pasw = input("Please enter your password: ")
-        if pasw == logins.c[1]:
-            print("Welcome")
-            print("ERR: Local Admin disabled by global administrator")
-        else:
-            print("Incorrect password.")
-            retry()
-    elif name.lower() == logins.d[0]:
-        print("Welcome,",name)
-        pasw = input("Please enter your password: ")
-        if pasw == logins.d[1]:
-            print("Why did I make you admin what no")
-            redoAO()
-        else:
-            print("Incorrect password.")
-            
-            retry()
-    elif name.lower() == logins.e[0]:
-        print("Welcome,",name)
-        pasw = input("Please enter your password: ")
-        if pasw == logins.e[1]:
-            print("Welcome")
-        else:
-            print("Incorrect password.")
-            retry()
-    elif name.lower() == logins.f[0]:
-        print("Welcome,",name)
-        pasw = input("Please enter your password: ")
-        if pasw == logins.f[1]:
-            print("Welcome")
-        else:
-            print("Incorrect password.")
-            retry()
-    else:
-        print("Sorry. Your username is invalid.")
-        retry()
+                
 
-retry()
+    class logins:
+        a = ["sam","password"]
+        b = ["admin","admin123"]
+        c = ["local","local"]
+        d = ["alex","yeetethsksksksksk"]
+        e = ["false","false"]
+        f = ["false","false"]
+
+    def retry():
+        name = input("What is your username?: ")
+
+        if name.lower() == logins.a[0]:
+             pasw = input("Please enter your password: ")
+             if pasw == logins.a[1]:
+                return("Welcome!")
+             else:
+                return("Incorrect password.")
+                retry()
+        elif name.lower() == logins.b[0]:
+            return("Welcome,",name)
+            pasw = input("Please enter your password: ")
+            if pasw == logins.b[1]:
+                return("Welcome")
+                adminloggedin = True
+                redoAO()
+            else:
+                return("Incorrect password.")
+                retry()
+        elif name.lower() == logins.c[0]:
+            return("Welcome,",name)
+            pasw = input("Please enter your password: ")
+            if pasw == logins.c[1]:
+                return("Welcome")
+                return("ERR: Local Admin disabled by global administrator")
+            else:
+                return("Incorrect password.")
+                retry()
+        elif name.lower() == logins.d[0]:
+            return("Welcome,",name)
+            pasw = input("Please enter your password: ")
+            if pasw == logins.d[1]:
+                return("Why did I make you admin what no")
+                redoAO()
+            else:
+                return("Incorrect password.")
+                
+                retry()
+        elif name.lower() == logins.e[0]:
+            return("Welcome,",name)
+            pasw = input("Please enter your password: ")
+            if pasw == logins.e[1]:
+                return("Welcome")
+            else:
+                return("Incorrect password.")
+                retry()
+        elif name.lower() == logins.f[0]:
+            return("Welcome,",name)
+            pasw = input("Please enter your password: ")
+            if pasw == logins.f[1]:
+                return("Welcome")
+            else:
+                return("Incorrect password.")
+                retry()
+        else:
+            return("Sorry. Your username is invalid.")
+            retry()
+
+    retry()
+
+if __name__ == "__main__":
+    app.run()
