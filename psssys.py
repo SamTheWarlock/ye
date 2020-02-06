679daae948591a1dcd6169f6ab1e4ba1cfa1ddf6
SamTheWarlock <43090976+SamTheWarlock@users.noreply.github.com>
2020-02-06 18:26:36 +0000

    Update psssys.py

--- psssys.py
+++ psssys.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def application():
-    testvar = "Yeet"
+    testvar = logic("test test")
     return testvar
 if __name__ == "__main__":
     app.run()
